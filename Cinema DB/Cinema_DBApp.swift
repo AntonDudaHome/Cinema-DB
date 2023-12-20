b4ab07d38622db628348418573c8420bd2b28ded
Anton Duda <anton.duda@lembergsolutions.com>
2023-12-20 20:52:55 +0200

    Initial Commit

---
+++ Cinema DB/Cinema_DBApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cinema_DBApp.swift
+//  Cinema DB
+//
+//  Created by Anton.Duda on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Cinema_DBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
