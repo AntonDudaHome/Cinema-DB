cdf75644b1e255fb8f6d9a59141fe7c920bc471e
Anton Duda <anton.duda@lembergsolutions.com>
2023-12-21 10:10:32 +0200

    Added Constants file

---
+++ Components/Constants/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  CinemaBase
+//
+//  Created by Anton.Duda on 20.10.2023.
+//
+
+import Foundation
+
+struct Constants {
+    struct TextConstants {
+        static let splashText = "Cinema Base"
+        static let signInText = "Sign In"
+        static let signUpText = "Sign Up"
+        static let noAccountText = "Don't have an account?"
+        static let haveAccountText = "Already have an account?"
+        static let welcomeDivider = "or"
+    }
+}
